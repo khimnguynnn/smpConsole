88e39c624bb106699958ad98aa84d49724ccb0af
khimnguynnn <khim.nguynn@gmail.com>
2023-12-11 10:27:47 +0700

    init

---
+++ test.py
@@ -0,0 +1,25 @@
+from smpConsole import Console
+
+Console.debug("test")
+Console.debug("[*] test")
+Console.debug("[+] test")
+Console.debug("[-] test")
+Console.debug("[/] test")
+
+Console.info("test")
+Console.info("[*] test")
+Console.info("[+] test")
+Console.info("[-] test")
+Console.info("[/] test")
+
+Console.printf("test")
+Console.printf("[*] test")
+Console.printf("[+] test")
+Console.printf("[-] test")
+Console.printf("[/] test")
+
+Console.printe("test")
+Console.printe("[*] test")
+Console.printe("[+] test")
+Console.printe("[-] test")
+Console.printe("[/] test")
